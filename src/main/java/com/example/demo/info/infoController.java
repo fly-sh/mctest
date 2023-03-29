1b910ee38a63aadd59159c66b7ece1f1e97f0123
sungheum-park <111420273+sungheum-park@users.noreply.github.com>
2023-03-29 13:29:14 +0900

    POST 수정

--- src/main/java/com/example/demo/info/infoController.java
+++ src/main/java/com/example/demo/info/infoController.java
@@ -1,6 +1,7 @@
 package com.example.demo.info;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class infoController {
 
-	@GetMapping("/test")
+	@PostMapping(value="test")
 	public String test() {
 		
 		System.out.println("/call/test call success");
