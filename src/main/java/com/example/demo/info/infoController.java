e96b42c9b966846a5f76eac749d47796e8048f95
sungheum-park <111420273+sungheum-park@users.noreply.github.com>
2023-04-03 14:02:11 +0900

    map change

--- src/main/java/com/example/demo/info/infoController.java
+++ src/main/java/com/example/demo/info/infoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.simple.JSONArray;
@@ -29,12 +30,13 @@ public class infoController {
 		return "result 200";
 	}
 	@RequestMapping(value="/save", method = {RequestMethod.POST})
-	public String save(@RequestBody CallModel infoVO) {
+	public String save(@RequestBody HashMap<String, Object> infoVO) {
 		
 //		JSONParser jsonParser = new JSONParser();
 //		JSONObject jsonObj = (JSONObject) jsonParser.parse(infoVO);
-		
-		System.out.println("/call/save call success :: "+infoVO.getPhoneNumber()+"|||"+infoVO.getMessage());
+
+		System.out.println("/call/save call success :: ");
+		System.out.println("infoVO :: "+infoVO);
 		return "result 200";
 	}
 }
