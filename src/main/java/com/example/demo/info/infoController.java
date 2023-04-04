92462771c18ad5924487c2a80810d474eaa7cc5e
sungheum-park <111420273+sungheum-park@users.noreply.github.com>
2023-04-04 16:22:48 +0900

    as

--- src/main/java/com/example/demo/info/infoController.java
+++ src/main/java/com/example/demo/info/infoController.java
@@ -47,68 +47,75 @@ public class infoController {
 		System.out.println("/call/test call success :: "+inArgumentsTmp.get("phoneNumber"));
 		System.out.println("/call/test call success :: "+t_data.get("message"));
 		
+		String snPhnum = "01020949987";
+		
+		String mbnum = inArgumentsTmp.get("phoneNumber").toString();	// 수싲번호
+		String msgCotn = t_data.get("message").toString();				// 내용
+			
+		//sms 발송
+		try {
+			sendSMS(snPhnum, mbnum, msgCotn);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return "result 200";
+	}
+	
+	public void sendSMS(String snPhnum,String mbnum,String msgCotn) {
 		// megabird api 호출
 		String serviceURL = "https://api.megabird.co.kr:8080";
 		
 		String token = "eyJhbGciOiJIUzM4NCJ9.eyJ0b2tlbkRpdiI6IjNyZCIsIm1lbWJlcklkIjoiNDc2YWRlYTE4NzVjNDI4ZGI3NGJmNjAwN2ZkYWM2MTUiLCJpc0FjY2Vzc1Rva2VuIjp0cnVlLCJpYXQiOjE2Nzc4MjcxNTksImV4cCI6MzIzMzAyNzE1OX0.j0YkDKb0ERHQiNulIaaj4EoCPYY5WvLTM0KQ6qNihLecGiZn912KC0NWFoeUM5cC";
 		
-		String mbnum = inArgumentsTmp.get("phoneNumber").toString();
-		String msgCotn = t_data.get("message").toString();
-		
-//		try {
-//			String apiURL = serviceURL + "/api/v1/openapi/sms";
-//			URL url = new URL(apiURL);
-//			HttpURLConnection con = (HttpURLConnection)url.openConnection();
-//			con.setRequestMethod("POST");
-//			con.setRequestProperty("Content-Type", "application/json");
-//			con.setRequestProperty("Authorization", token);
-//			
-//			// 메시지 수신자
-//			JSONArray messageReceiverList = new JSONArray();
-//			JSONObject messageReceiver = new JSONObject();
-//			messageReceiver.put("mbnum", "01034563456");
-//			messageReceiver.put("tgusNm", "김나라");
-//			messageReceiverList.add(messageReceiver);
-//			
-//			// 메시지 항목
-//			JSONObject params = new JSONObject();
-//			params.put("svcKndCd", "SMS");
-//			params.put("msgTtl", "메시지제목");
-//			params.put("msgCotn", "메시지내용\n확인해주세요.");
-//			params.put("adIncluYn", "N");
-//			params.put("snPhnum", "01012341234");
-//			params.put("messageReceiverList", messageReceiverList);
-//			
-//			con.setDoOutput(true);
-//			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-//			wr.write(params.toString().getBytes(StandardCharsets.UTF_8));
-//			wr.flush();
-//			wr.close();
-//
-//			int responseCode = con.getResponseCode();
-//			BufferedReader br;
-//			if(responseCode==200) { // 정상 호출
-//				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-//			} else {  // 에러 발생
-//				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-//			}
-//
-//			String inputLine;
-//			StringBuffer response = new StringBuffer();
-//			while ((inputLine = br.readLine()) != null) {
-//				response.append(inputLine);
-//			}
-//			br.close();
-//			System.out.println(response.toString());
-//			
-//		} catch (Exception e) {
-//			System.out.println(e);
-//		}
-		
-		
-		return "result 200";
+		try {
+			String apiURL = serviceURL + "/v1/openapi/sms/send";
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Authorization", token);
+			
+			// 메시지 수신자
+			JSONArray messageReceiverList = new JSONArray();
+			JSONObject messageReceiver = new JSONObject();
+			messageReceiver.put("mbnum", mbnum);
+			messageReceiverList.add(messageReceiver);
+			
+			// 메시지 항목
+			JSONObject params = new JSONObject();
+			params.put("svcKndCd", "SMS");
+			params.put("msgCotn", msgCotn);
+			params.put("adIncluYn", "N");
+			params.put("snPhnum", snPhnum);
+			params.put("messageReceiverList", messageReceiverList);
+			
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.write(params.toString().getBytes(StandardCharsets.UTF_8));
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if(responseCode==200) { // 정상 호출
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {  // 에러 발생
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+			System.out.println(response.toString());
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-	
 	
 	@RequestMapping(value="/save", method = {RequestMethod.POST})
 	public String save(@RequestBody HashMap<String, Object> infoVO) {
